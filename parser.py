5430b70bee1a65aef6899a1868c678b64b205d23
horazio <hfusco@stuy.edu>
2019-04-05 15:42:24 -0400

    testing

--- parser.py
+++ parser.py
@@ -113,10 +113,12 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
                     float(args[0]), float(args[1]), float(args[2]),
                     float(args[3]), float(args[4]), float(args[5]))
 
+
+
             matrix_mult(transform, temp)
             draw_polygons(temp, screen, color)
-            temp = []
 
+            temp = []
         elif line == 'circle':
             #print 'CIRCLE\t' + str(args)
             add_circle(edges,
@@ -154,7 +156,7 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
         elif line == 'scale':
             #print 'SCALE\t' + str(args)
             t = make_scale(float(args[0]), float(args[1]), float(args[2]))
-            matrix_mult(t, transform)
+            matrix_mult(t, transform )
 
         elif line == 'move':
             #print 'MOVE\t' + str(args)
@@ -186,9 +188,9 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
             polygons = []
 
         elif line == 'display' or line == 'save':
-            clear_screen(screen)
-            draw_lines(edges, screen, color)
-            draw_polygons(polygons, screen, color)
+            #clear_screen(screen)
+            #draw_lines(edges, screen, color)
+            #draw_polygons(polygons, screen, color)
 
             if line == 'display':
                 display(screen)
